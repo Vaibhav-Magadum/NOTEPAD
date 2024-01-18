a9a88d636474f561e7550dd3a426a1737d8986c8
Vaibhav-Magadum <vaibhavmagadum859@gmail.com>
2024-01-18 22:38:52 +0530

    .

--- main.py
+++ main.py
@@ -14,6 +14,22 @@ def open_file():
             text_area.delete("1.0", tk.END)
             text_area.insert(tk.END, file.read())
 
+def copy_text():
+    selected_text = text_area.get(tk.SEL_FIRST, tk.SEL_LAST)
+    root.clipboard_clear()
+    root.clipboard_append(selected_text)
+
+def cut_text():
+        selected_text = text_area.get(tk.SEL_FIRST, tk.SEL_LAST)
+        root.clipboard_clear()
+        root.clipboard_append(selected_text)
+        text_area.delete(tk.SEL_FIRST, tk.SEL_LAST)
+
+def paste_text():
+    selected_text = root.clipboard_get()
+    text_area.insert(tk.INSERT, selected_text)
+
+
 root = tk.Tk()
 root.title("Notepad")
 
@@ -28,5 +44,12 @@ file_menu.add_separator()
 file_menu.add_command(label="Exit", command=root.quit)
 menu_bar.add_cascade(label="File", menu=file_menu)
 
+edit_bar = tk.Menu(root)
+edit_bar.add_command(label="Cut", command=cut_text)
+edit_bar.add_command(label="Copy", command=copy_text)
+edit_bar.add_command(label="Paste", command=paste_text)  # Add the command for Paste
+menu_bar.add_cascade(label="Edit", menu=edit_bar)
+
 root.config(menu=menu_bar)
 root.mainloop()
+
